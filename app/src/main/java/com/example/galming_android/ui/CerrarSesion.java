8f9cb5dac93a748793571c8c77743fadbd16ba49
iMario44 <90375304+iMario44@users.noreply.github.com>
2021-11-19 13:14:24 +0100

    Con cerrar sesion

--- app/src/main/java/com/example/galming_android/ui/CerrarSesion.java
+++ app/src/main/java/com/example/galming_android/ui/CerrarSesion.java
@@ -2,14 +2,40 @@ package com.example.galming_android.ui;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.example.galming_android.MainActivity;
 import com.example.galming_android.R;
 
 public class CerrarSesion extends Fragment {
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Bundle bundle = new Bundle();
+        bundle.putInt("layout", R.layout.fragment_home);
+        ((MainActivity)getContext()).cambiarFragmento(R.id.nav_home,bundle);
+        ((MainActivity)getContext()).estadoLogin(false);
+        Toast.makeText(getContext(), "Sesion cerrada", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
 }

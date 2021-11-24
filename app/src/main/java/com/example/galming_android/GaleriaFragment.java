0ddddf20258a6a5361ee98a2b70419a42ea0be8f
Inushin <57062736+Inushin@users.noreply.github.com>
2021-11-24 14:05:10 +0100

    Galería OKKKKK

--- app/src/main/java/com/example/galming_android/GaleriaFragment.java
+++ app/src/main/java/com/example/galming_android/GaleriaFragment.java
@@ -1,70 +1,54 @@
 package com.example.galming_android;
 
+
+import android.content.Context;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link GaleriaFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+
 public class GaleriaFragment extends Fragment
 {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public GaleriaFragment()
-    {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment GaleriaFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static GaleriaFragment newInstance(String param1, String param2)
-    {
-        GaleriaFragment fragment = new GaleriaFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
-    public void onCreate(Bundle savedInstanceState)
+    public void onAttach(@NonNull Context context)
     {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null)
-        {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        super.onAttach(context);
     }
 
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState)
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
     {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_galeria, container, false);
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState)
+    {
+        super.onViewCreated(view, savedInstanceState);
+
+        final ImageView ivGaleria1 = view.findViewById(R.id.ivGaleria1);
+        final ImageView ivGaleria2 = view.findViewById(R.id.ivGaleria2);
+        final ImageView ivGaleria3 = view.findViewById(R.id.ivGaleria3);
+        final ImageView ivGaleria4 = view.findViewById(R.id.ivGaleria4);
+        final ImageView ivGaleria5 = view.findViewById(R.id.ivGaleria5);
+        final ImageView ivGaleria6 = view.findViewById(R.id.ivGaleria6);
+        final ImageView ivGaleria7 = view.findViewById(R.id.ivGaleria7);
+
+        ivGaleria1.setImageResource(R.drawable.galeria1);
+        ivGaleria2.setImageResource(R.drawable.galeria2);
+        ivGaleria3.setImageResource(R.drawable.galeria3);
+        ivGaleria4.setImageResource(R.drawable.galeria4);
+        ivGaleria5.setImageResource(R.drawable.galeria5);
+        ivGaleria6.setImageResource(R.drawable.galeria6);
+        ivGaleria7.setImageResource(R.drawable.galeria7);
     }
 }
